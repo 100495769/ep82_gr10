2c17b684e78a98c9835fc55d74e56594b515068f
100494620 <100494620@alumnos.uc3m.es>
2024-10-22 23:24:45 +0200

    new style file + popup window iniciar session

---
+++ script.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("popupIniciar").style.display = "block";
+    document.body.style.overflow = "hidden";
+}
+
+function closeForm() {
+    document.getElementById("popupIniciar").style.display = "none";
+    document.body.style.overflow = "";
+}
