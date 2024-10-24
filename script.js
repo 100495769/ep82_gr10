0a80fec5b52b6d9d266c1202d3bbe88b3f6b3e13
100494620 <100494620@alumnos.uc3m.es>
2024-10-24 10:25:44 +0200

    library for jquery + functioning switch to login

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 function openSignInForm() {
     document.getElementById("popupIniciar").style.display = "block";
     document.body.style.overflow = "hidden";
@@ -7,6 +8,8 @@ function openSignInForm() {
 function closeSignInForm() {
     document.getElementById("popupIniciar").style.display = "none";
     document.body.style.overflow = "";
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
 }
 
 // this part should be actually in the users.json but since we dont have
@@ -22,6 +25,7 @@ const usersString = JSON.stringify(users);
 
 localStorage.setItem("userData", usersString);
 let isLoggedIn = false;
+
 function checkItem() {
     let enteredUsername = document.getElementById("username").value;
     let enteredPassword = document.getElementById("password").value;
@@ -33,31 +37,24 @@ function checkItem() {
 
         if (enteredUsername in obj.users) {
             if (obj.users[enteredUsername] === enteredPassword) {
-                window.confirm("You are registered!");
+                //window.confirm("You are registered!");
                 isLoggedIn = true;
+                $('#signIn, #registrar').remove();
+                $('#Navegacion').append(`
+                    <a onclick="openSignInForm()">
+                        <img class="login-button"  src="photos/login-icon.png" alt="login icon">
+                    </a>
+                `);
+                closeSignInForm();
             } else {
                 alert("Wrong password!");
+                document.getElementById("password").value = "";
             }
         } else {
             alert("You are not registered!");
+            document.getElementById("password").value = "";
         }
     }
+    return false;
 }
 
-//experimenting, it is not working yet
-let signIn = document.getElementById('Navegacion');
-
-function changeContent() {
-    signIn.innerHTML = '<a href="home.html#Inicio">Inicio</a>\n' +
-        '    <a href="home.html#PapaNoel">Papá Noel</a>\n' +
-        '    <a href="home.html#LasCartas">¡Las Cartas!</a>\n' +
-        '    <a href="home.html#EnviaTuCarta">Envía tu Carta</a>\n' +
-        '    <a href="home.html#Juega">Juega</a>' +
-        '    <a href="home.html#EnviaTuCarta">\n' +
-        '        <img class="icon" src="photos/login-icon.png" alt="login icon">\n' +
-        '     </a>';
-}
-
-if (isLoggedIn === true) {
-    changeContent();
-}
